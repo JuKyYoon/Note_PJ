f1c24543558c56a6474e88bd538b95c7008be295
jky <yoongh97@naver.com>
2017-04-02 23:48:09 +0900

    update everything

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,29 +2,39 @@ import React from 'react';
 import Neditor from '../components/neditor'
 import './Home.css'
 import {Button,ButtonGroup,PageHeader} from 'react-bootstrap';
-import {FormGroup,ControlLabel,FormControl,HelpBlock} from 'react-bootstrap';
+import {ListGroupItem, ListGroup} from 'react-bootstrap';
+import {FormGroup,FormControl} from 'react-bootstrap';
+function alertClicked() {
+  alert('You clicked the third ListGroupItem');
+}
 
 const Home = () => {
     return (
         <div className="HomePage">
             <div className="SideMenu">
                 <ButtonGroup vertical block>
-                    <Button>Full width button</Button>
-                    <Button>Full width button</Button>
-                    <Button>Full width button</Button>
-                    <Button>Full width button<  /Button>
-                    <Button>Full width button</Button>
-                    <Button>Full width button</Button>
-                    <Button>Full width button</Button>
+                    <ListGroup>
+                      <div className="home_search">
+                        <FormControl className="home_input_search" type="text" placeholder="Search" />
+                        <button className="homesearchsubmit" type="submit">Submit</button>
+                      </div>
+                      <ListGroupItem header="New Memo" href="#link1">Link 1</ListGroupItem>
+                      <ListGroupItem header="memo 1" href="#link2">Link 2</ListGroupItem>
+                      <ListGroupItem header="memo 2" onClick={alertClicked}>
+                        Trigger an alert
+                      </ListGroupItem>
+                    </ListGroup>
                 </ButtonGroup>
             </div>
 
             <div className="editspace">
-                <div className="savebutton">
+                <div className="inputtitle">
                     <PageHeader>
                         <input type="text" name="name" placeholder="Enter a title"/>
-                        <small>Writer</small>
-                        <Button bsStyle="success">Save</Button>
+                        <small className="writer">Writer</small>
+                        <div className="savebuttonspace">
+                            <Button bsStyle="success">Save</Button>
+                        </div>
                     </PageHeader>
                 </div>
 
