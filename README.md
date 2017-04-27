7bd1374fc2f121f560ff955415013051291be25f
jky <yoongh97@naver.com>
2017-04-27 13:38:19 +0900

    update readme.md

--- README.md
+++ README.md
@@ -1,9 +1,40 @@
-This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+# 2017 Note App
+2학년 2016003727 윤주경
+
 
 ## Table of Contents
 -Home
 ![home](https://github.com/JuKyYoon/Note_PJ/blob/master/example_image/Home.PNG)
+
 -Account
-![account](https://github.com/JuKyYoon/Note_PJ/blob/master/example_image/Account.PNG)
+ Not yet
+
+-Setting
+ Not yet
+
 -View
-![account](https://github.com/JuKyYoon/Note_PJ/blob/master/example_image/View.PNG)
+ Not yet
+
+
+
+## Home
+* draft.js를 사용한 에디터 구현
+* 텍스트 꾸미기 가능 (Bold,Italic,Underline,Monospace)
+* 목차 기능 추가
+* 검색기능과 사이드메뉴는 추후 추가 예정
+* mongoDB와 연동시켜 저장 가능
+* 상단바를 통해 다른 메뉴로 이동 가능
+
+
+## Account
+* 파일 백업기능 추가 예정...
+
+## View
+* 지금까지 작성한 모든 노트 파일 볼 수 있고 누르면 draft.js로 작성한 그대로 볼 수 있는 기능
+* 수정 버튼 누르면 home화면으로 이동해 수정 가능한 기능
+* 시간 혹은 이름순 정렬 가능한 기능
+* 을 추가할 예정
+
+## Setting
+* 전체적인 노트 색깔을 변경 가능하게 할 기능
+* 을 추가할 것입니다.
--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser'); //JSON형태로 변환
 var autoIncrement = require("mongoose-auto-increment");
-var app = express();
+var noteapp = express();
 var db = mongoose.connection;
 db.on('error', console.error);
 db.once('open', function(){
@@ -29,7 +29,7 @@ var MemoSchema = new Schema({
 MemoSchema.plugin(autoIncrement.plugin, '   Memo');
 var Memo = mongoose.model('Memo', MemoSchema);
 
-app.post('/view', function(req,res){
+noteapp.post('/view', function(req,res){
     Memo.find(function(err, memos){
         if(err) {return res.status(500).send({error: 'database failure'});}
         res.json(memos);
@@ -42,7 +42,7 @@ app.post('/view', function(req,res){
 
 
 
-app.use(bodyParser.urlencoded({ extended: false }))
+noteapp.use(bodyParser.urlencoded({ extended: false }))
 
 // app.get('/', (req, res) => {
     // Memo.find((err, docs) => {
@@ -54,7 +54,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
     // res.render('view');
 // });
 
-app.post('/view2', (req, res) => {
+noteapp.post('/view2', (req, res) => {
     var newMemo = new Memo();
     // newMemo.title = req.body.title;
     // console.log(req.body.title);
@@ -99,68 +99,68 @@ app.post('/view2', (req, res) => {
 
 
 //기본 설정 파일
-app.use('/', express.static(path.resolve(__dirname, '../build')));
+noteapp.use('/', express.static(path.resolve(__dirname, './build')));
 
 //static경로를 예외로 처리하고 리액트 index.html을 보여준다.
-app.get('*', (req, res, next) => {
+noteapp.get('*', (req, res, next) => {
     if(req.path.split('/')[1] === 'static') return next();
-    res.sendFile(path.resolve(__dirname, '../build/index.html'));
+    res.sendFile(path.resolve(__dirname, './build/index.html'));
 });
 
-app.listen(4000, function () {
+noteapp.listen(4000, function () {
   console.log('AAAAAAAAAAAAAAAAAAAA');
 });
 
 
-
-const {napp, BrowserWindow} = require('electron')
-const path = require('path')
+const {app, BrowserWindow} = require('electron')
+// const path = require('path')
 const url = require('url')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+// 윈도우 객체를 전역에 유지합니다. 만약 이렇게 하지 않으면
+// 자바스크립트 GC가 일어날 때 창이 멋대로 닫혀버립니다.
 let win
 
 function createWindow () {
-  // Create the browser window.
+  // 새로운 브라우저 창을 생성합니다.
   win = new BrowserWindow({width: 800, height: 600})
 
-  // and load the index.html of the app.
+  // 그리고 현재 디렉터리의 index.html을 로드합니다.
   win.loadURL(url.format({
     pathname: 'localhost:4000',
     protocol: 'http:',
     slashes: true
   }))
 
-  // Open the DevTools.
-  win.webContents.openDevTools()
 
-  // Emitted when the window is closed.
+  // 개발자 도구를 엽니다.
+  // win.webContents.openDevTools()
+
+  // 창이 닫히면 호출됩니다.
   win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
+    // 윈도우 객체의 참조를 삭제합니다. 보통 멀티 윈도우 지원을 위해
+    // 윈도우 객체를 배열에 저장하는 경우가 있는데 이 경우
+    // 해당하는 모든 윈도우 객체의 참조를 삭제해 주어야 합니다.
     win = null
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-napp.on('ready', createWindow)
+// 이 메서드는 Electron의 초기화가 끝나면 실행되며 브라우저
+// 윈도우를 생성할 수 있습니다. 몇몇 API는 이 이벤트 이후에만
+// 사용할 수 있습니다.
+app.on('ready', createWindow)
 
-// Quit when all windows are closed.
-napp.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
+// 모든 창이 닫히면 애플리케이션 종료.
+app.on('window-all-closed', () => {
+  // macOS의 대부분의 애플리케이션은 유저가 Cmd + Q 커맨드로 확실하게
+  // 종료하기 전까지 메뉴바에 남아 계속 실행됩니다.
   if (process.platform !== 'darwin') {
-    napp.quit()
+    app.quit()
   }
 })
 
-napp.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
+app.on('activate', () => {
+  // macOS에선 보통 독 아이콘이 클릭되고 나서도
+  // 열린 윈도우가 없으면, 새로운 윈도우를 다시 만듭니다.
   if (win === null) {
     createWindow()
   }
